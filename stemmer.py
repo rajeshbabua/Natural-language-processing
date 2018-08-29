c2b26164f2bbd384a13226656ad2525f1fa81353
Rajesh_QM&SB <32333617+rajeshbabua@users.noreply.github.com>
2018-08-29 17:13:59 +0530

    Update stemmer.py

--- stemmer.py
+++ stemmer.py
@@ -2,5 +2,6 @@ import nltk
 from nltk.stem import PorterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
 words = ["python", "pythonnning", "programme", "program" ]
+ps = PorterStemmer()
 for w in words:
-  print (PorterStemmer.stem(w))
+  print (ps.stem(w))
