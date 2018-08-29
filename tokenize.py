4dc982edb428355cd43717b611cd022e7efa086d
Rajesh_QM&SB <32333617+rajeshbabua@users.noreply.github.com>
2018-08-29 16:16:55 +0530

    Update tokenize.py

--- tokenize.py
+++ tokenize.py
@@ -1,4 +1,5 @@
 import nltk
+# nltk.download()-----> use  this to downlaod all packages required for nltk 
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
 text = "This tutorial is meant for those who want to get to know the Flow of TensorFlow. Ideally, you already know some of the Tensor of TensorFlow."
