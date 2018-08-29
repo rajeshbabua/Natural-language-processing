83125adcb3a32ab5248754c1cf605a56c78ce5fa
Rajesh_QM&SB <32333617+rajeshbabua@users.noreply.github.com>
2018-08-29 21:48:42 +0530

    Update pos.py

--- pos.py
+++ pos.py
@@ -1,7 +1,6 @@
 import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize 
-text="""the time of the dayb isver good and i dont not how itwas a wonmderful opportunity 
-so uimeantitajdfuj the way i w"""
+text= input("enter a sentence. you will it part of speech for each word")
 t = nltk.word_tokenize(text)
 for r in t:
   x = nltk.pos_tag(t)
