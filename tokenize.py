ce5d1a635067c7b5403bc073a2c75fffb78cd506
Rajesh_QM&SB <32333617+rajeshbabua@users.noreply.github.com>
2018-08-29 17:04:21 +0530

    Update tokenize.py

--- tokenize.py
+++ tokenize.py
@@ -3,12 +3,7 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
 text = "This tutorial is meant for those who want to get to know the Flow of TensorFlow. Ideally, you already know some of the Tensor of TensorFlow."
-s_w = set(stopwords.words("english")
 sents = sent_tokenize(text)
-f_s = []
-for w in words:
-    if w not in s_w:
-         f_s.append(w)
-print (s_w)                   
 print ("sents")
-print(f_s)
+words = word_tokenize(text)
+print ("words")
